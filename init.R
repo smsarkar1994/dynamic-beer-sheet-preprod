69a68db53324d180d47e3fd236220c95cdfd7413
smsarkar1994 <60992047+smsarkar1994@users.noreply.github.com>
2022-08-25 23:06:21 -0400

    Add files via upload

--- init.R
+++ init.R
@@ -1,2 +1,2 @@
-helpers.installPackages("dplyr", "rvest", "httr", "jsonlite", "stringr", "ffscrapr",
+helpers.installPackages("dplyr", "rvest", "httr", "jsonlite", "stringr", "ffscrapr", "RSelenium",
                         "glue", "purrr", "readxl", "shiny", "googlesheets4")
